7b58267846f74b3a22378938850057856b48bebd
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2018-10-30 23:09:47 +0200

    Comit3
    Upload ArraySort

---
+++ Prom_begin/src/test/ArraySort.java
@@ -0,0 +1,22 @@
+package test;
+
+public class ArraySort {
+
+	public static void main(String[] args) {
+		// Сортировка массива "пузырьком"
+		int[] array = {30, 2, 10, 4, 6};
+		int length = array.length;
+		int a, b, c;
+		for (a = 1; a < length; a++)
+		for (b = length - 1; b >= a; b--) {
+        if (array[b - 1] > array[b]) {
+        c = array[b - 1];
+        array[b - 1] = array[b];
+        array[b] = c;
+		}
+    }
+		for (int i = 0; i < length; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+}
