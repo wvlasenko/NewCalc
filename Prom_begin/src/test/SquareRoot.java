c6f2443d132948d77a493bc21d99092aeb4533d5
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2018-10-30 23:04:20 +0200

    Comit2
    UploadSquareRoot

--- Prom_begin/src/test/SquareRoot.java
+++ Prom_begin/src/test/SquareRoot.java
@@ -3,30 +3,26 @@ package test;
 public class SquareRoot {
 
 	public static void main(String[] args) {
-		double a = 0;
-		double b = 10;
-		float c = 0;
+		double a = 3;
+		double b = 2.5;
+		double c = -0.5;
 		double x1;
 		double x2;
 		double D;
-	
 		D = b * b - 4 * a * c;
-		if ((D > 0)&&(a!=0)&&(b!=0)) {
+		if (D > 0) {
 			x1 = (-b + Math.sqrt(D)) / (2 * a);
 			x2 = (-b - Math.sqrt(D)) / (2 * a);
 			System.out.println("x1=" + x1);
 			System.out.println("x2=" + x2);
-		} else if ((D == 0)??(a==0)){
-			double x = -b/(2*a);
-			
-			System.out.println("x1="+x);
-			System.out.println("x2="+x);{
-		
-			}
-			
+		} else if (D == 0) {
+			//x1 =  -b / (2 * a);
+			System.out.println("x1 = " + (-b/2*a));
+			System.out.println("x2 = " + (-b/2*a));
+
 		} else {
-			System.out.println("x1 ="+"");
-			System.out.println("x2 ="+"");
+			System.out.println("x1 = "+"");
+			System.out.println("x2 = "+"");
 		}
 	}
 }
