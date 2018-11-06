5c2a50b9df4cccb699e5607e55cecd19b6ae22f1
Camaradas_CC <Camaradas_CC@Camacolor>
2018-11-06 13:13:32 +0200

    Comit3
    add sort

---
+++ Prom_begin/src/test/ArrSortBinary.java
@@ -0,0 +1,41 @@
+package test;
+
+public class ArrSortBinary {
+
+	public static void main(String[] args) {
+/*
+
+Напишіть застосування, що виконує пошук заданого числа 
+у відсортованому масиві — бінарний пошук
+У випадку коли число знайдено виведіть на екран 
+його позицію в масиві (позиції нумеруємо з нуля) або -1 в іншому випадку
+*/
+		int data[] = {101, 21, 29, 31, 48, 55, 62, 63, 67, 71, 91};
+		int numberToFind = 101;
+		int firstNumb = data[0];
+		int firstIndex;
+		int lastNumb = data[data.length - 1];
+		if (firstNumb < lastNumb) {
+			for (int i = 0; i < data.length; i++) {
+				int val = data[i];
+				firstIndex=0;
+				if (val == numberToFind) {
+					System.out.println(i+firstIndex);
+
+				}
+			}
+		}
+		if (firstNumb > lastNumb) {
+			for (int i=data.length-1;i>=0;i--) {
+				int val = data[i];
+				firstIndex=-1;
+				if (val == numberToFind) {
+					System.out.println(i+firstIndex);
+				}
+			}
+		}
+	}
+}
+/*Неправильно обрахований результат. Для набору вхідних параметрів [101, 21, 29, 31, 48, 55, 62, 63, 67, 71, 91] очікуваний результат: -1. Ваш результат: ''
+У тестовому наборі перший елемент - число, яке необхідно знайти
+*/
