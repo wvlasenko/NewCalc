c049137d019db461cee491f4306048034b4265bf
volodymyr <wvlasenko@gmail.com>
2018-11-17 07:16:46 +0200

    add matrix

---
+++ Prom_begin/src/test/MatrixPrint.java
@@ -0,0 +1,16 @@
+package test;
+
+public class MatrixPrint {
+    public static void main(String[] args) {
+        int [] mFirst = {2,3,4};
+        int [] mTwo = {6,8,10};
+        for (int i = 0; i < mFirst.length; i++){
+            System.out.print("\t"+mFirst[i]);
+           // System.out.println("\n");
+            System.out.print("\t"+ mTwo[i]);
+        }
+
+
+
+    }
+}
